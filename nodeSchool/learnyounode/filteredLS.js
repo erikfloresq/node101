f176fa65c10aa5f5d6f53446869f7c0debc0a7d3
Erik Flores <erikfloresq@gmail.com>
2018-11-03 05:41:02 -0500

    Add filteredLS

---
+++ nodeSchool/learnyounode/filteredLS.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = process.argv[2];
+const extension = '.' + process.argv[3];
+
+fs.readdir(filePath, function(err, list) {
+  if (err) {
+      return console.error(err);
+  }
+  list.forEach(function(item) {
+    if (path.extname(item) == extension) {
+      console.log(item);
+    }
+  });
+});
